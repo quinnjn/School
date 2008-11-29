quinnjn/School

commit 61ce3484645c8670a360c6cabeead5280b1723ae

Replaced the use of deprecated MiscUtilities.MenuItemCompare with a new preferred alternative; org.gjt.sp.jedit.menu.MenuItemTextComparator. (SF.net patch #2308329)
git-svn-id: http://jedit.svn.sourceforge.net/svnroot/jedit/jEdit/trunk@14107 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/MiscUtilities.java
@@ -46,7 +46,7 @@ import org.gjt.sp.util.StandardUtilities;
 import org.gjt.sp.util.IOUtilities;
 
 import org.gjt.sp.util.XMLUtilities;
-import org.gjt.sp.jedit.menu.EnhancedMenuItem;
+import org.gjt.sp.jedit.menu.MenuItemTextComparator;
 //}}}
 
 /**
@@ -1431,22 +1431,16 @@ loop:		for(;;)
 	//{{{ MenuItemCompare class
 	/**
 	 * Compares menu item labels.
+	 * @deprecated Replaced with {@link org.gjt.sp.menu.compare.MenuItemTextComparator}
 	 */
 	@Deprecated
 	public static class MenuItemCompare implements Compare
 	{
+		private MenuItemTextComparator comparator = new MenuItemTextComparator();
+		
 		public int compare(Object obj1, Object obj2)
 		{
-			boolean obj1E, obj2E;
-			obj1E = obj1 instanceof EnhancedMenuItem;
-			obj2E = obj2 instanceof EnhancedMenuItem;
-			if(obj1E && !obj2E)
-				return 1;
-			else if(obj2E && !obj1E)
-				return -1;
-			else
-				return StandardUtilities.compareStrings(((JMenuItem)obj1).getText(),
-					((JMenuItem)obj2).getText(),true);
+			return comparator.compare((JMenuItem)obj1, (JMenuItem)obj2);
 		}
 	} //}}}
 

org/gjt/sp/jedit/browser/BrowserCommandsMenu.java
@@ -30,6 +30,7 @@ import javax.swing.*;
 
 import org.gjt.sp.jedit.io.*;
 import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.menu.MenuItemTextComparator;
 //}}}
 
 /**
@@ -242,7 +243,7 @@ public class BrowserCommandsMenu extends JPopupMenu
 		}
 
 		Collections.sort(encodingMenuItemList,
-			new MiscUtilities.MenuItemCompare());
+			new MenuItemTextComparator());
 
 		Iterator iter = encodingMenuItemList.iterator();
 		while(iter.hasNext())

org/gjt/sp/jedit/browser/VFSBrowser.java
@@ -40,6 +40,7 @@ import org.gjt.sp.jedit.search.*;
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.jedit.buffer.JEditBuffer;
 import org.gjt.sp.util.Log;
+import org.gjt.sp.jedit.menu.MenuItemTextComparator;
 //}}}
 
 /**
@@ -951,7 +952,7 @@ public class VFSBrowser extends JPanel implements EBComponent,
 
 		if (!vec.isEmpty())
 		{
-			Collections.sort(vec,new MiscUtilities.MenuItemCompare());
+			Collections.sort(vec,new MenuItemTextComparator());
 			for(int i = 0; i < vec.size(); i++)
 				pluginMenu.add(vec.get(i));
 		}

org/gjt/sp/jedit/menu/MacrosProvider.java
@@ -80,7 +80,7 @@ public class MacrosProvider implements DynamicMenuProvider
 			}
 		}
 
-		Collections.sort(menuItems,new MiscUtilities.MenuItemCompare());
+		Collections.sort(menuItems, new MenuItemTextComparator());
 		for(int i = 0; i < menuItems.size(); i++)
 		{
 			menu.add((JMenuItem)menuItems.get(i));

org/gjt/sp/jedit/menu/MenuItemTextComparator.java
@@ -1,0 +1,65 @@
+/*
+ * MenuItemTextComparator.java - Compares the text values of two JMenuItems
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2008 Eric Berry
+ * Portions copyright (C) 1999, 2005 Slava Pestov
+ * Portions copyright (C) 2000 Richard S. Hall
+ * Portions copyright (C) 2001 Dirk Moebius
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.gjt.sp.jedit.menu;
+
+//{{{ Imports
+import java.util.Comparator;
+
+import javax.swing.JMenuItem;
+
+import org.gjt.sp.util.StandardUtilities;
+
+//}}}
+/**
+ * MenuItemTextComparator implements java.util.Comparator, and compares the text
+ * value of JMenuItems using the case-insensitive smart comparison of
+ * StandardUtilities.compareStrings. If one of the JMenuItems is an
+ * EnhancedMenuItem it is given a higher comparison value.
+ */
+public class MenuItemTextComparator implements Comparator<JMenuItem>
+{
+
+	// {{{ Compare Method.
+	public int compare(JMenuItem obj1, JMenuItem obj2)
+	{
+		int compareValue = 0;
+		boolean obj1E = obj1 instanceof EnhancedMenuItem;
+		boolean obj2E = obj2 instanceof EnhancedMenuItem;
+		if (obj1E && !obj2E)
+		{
+			compareValue = 1;
+		}
+		else if (obj2E && !obj1E)
+		{
+			compareValue = -1;
+		}
+		else
+		{
+			compareValue = StandardUtilities.compareStrings(obj1.getText(), obj2.getText(), true);
+		}
+		return compareValue;
+	} // }}}
+
+}

org/gjt/sp/jedit/menu/PluginsProvider.java
@@ -105,8 +105,7 @@ public class PluginsProvider implements DynamicMenuProvider
 		for(int i = 0; i < letters.length; i++)
 		{
 			List list = letters[i];
-			Collections.sort(list,new MiscUtilities
-				.MenuItemCompare());
+			Collections.sort(list,new MenuItemTextComparator());
 		}
 
 		int maxItems = jEdit.getIntegerProperty("menu.spillover",20);

org/gjt/sp/jedit/menu/RecentDirectoriesProvider.java
@@ -118,7 +118,7 @@ public class RecentDirectoriesProvider implements DynamicMenuProvider
 		if(sort)
 		{
 			Collections.sort(menuItems,
-				new MiscUtilities.MenuItemCompare());
+					new MenuItemTextComparator());
 			for(int i = 0; i < menuItems.size(); i++)
 			{
 				if(menu.getMenuComponentCount() >= maxItems

org/gjt/sp/jedit/menu/RecentFilesProvider.java
@@ -164,7 +164,7 @@ public class RecentFilesProvider implements DynamicMenuProvider
 
 		if(sort)
 		{
-			Collections.sort(menuItems, new MiscUtilities.MenuItemCompare());
+			Collections.sort(menuItems, new MenuItemTextComparator());
 			for(int i = 0; i < menuItems.size(); i++)
 			{
 				if(menu.getMenuComponentCount() >= maxItems

commit 10385a93205f4b93b3d4d0d29c230018b1a74959

Fixed a javadoc warning about a wrong @link tag introduced in r14107.
git-svn-id: http://jedit.svn.sourceforge.net/svnroot/jedit/jEdit/trunk@14111 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/MiscUtilities.java
@@ -1431,7 +1431,7 @@ loop:		for(;;)
 	//{{{ MenuItemCompare class
 	/**
 	 * Compares menu item labels.
-	 * @deprecated Replaced with {@link org.gjt.sp.menu.compare.MenuItemTextComparator}
+	 * @deprecated Replaced with {@link org.gjt.sp.jedit.menu.MenuItemTextComparator}
 	 */
 	@Deprecated
 	public static class MenuItemCompare implements Compare