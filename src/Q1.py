01f7fa0979cf7e9bb593032e33b776c8a83b8689
quinnjn <quinn@neumiiller.com>
2013-03-11 15:55:20 -0600

    Reads in the gameboard and generates it!

--- src/Q1.py
+++ src/Q1.py
@@ -7,7 +7,48 @@
 ###############################################################################
 #IMPORTS
 ###############################################################################
+from Tkinter import *
+import sys
+
+
+###############################################################################
+#CLASS
+###############################################################################
+
+class GameBoard:
+    def __init__(self, textBoard):
+        master = Tk()
+        master.title("Wumpus' Dungeon")
+        SIZE = int(textBoard.pop(0))
+
+        print textBoard
+
+
+        for x in range(SIZE):
+            for y in range(SIZE):
+
+                l = Label(
+                    master,
+                    text= textBoard[x][y] +"\n",
+                    width=10,
+                    height=5
+                ).grid(
+                    row=x, 
+                    column=y
+                )       
+        self.master = master
+
+    def loop(self):
+        self.master.mainloop() 
 
 ###############################################################################
 #FUNCTIONS
 ###############################################################################
+
+fileLoc = sys.argv[1]
+f = open(fileLoc, 'r')
+
+file = f.read().strip().split("\n")
+
+gb = GameBoard(list(file))
+gb.loop()
